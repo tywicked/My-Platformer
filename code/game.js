eed745ac68cf212aed8d0b7c3d0ae0f3f8ee4013
tywicked <tyjohn@live.com>
2016-10-16 22:38:44 -0400

    Lab5

--- code/game.js
+++ code/game.js
@@ -39,7 +39,7 @@ function Level(plan) {
       else if (ch == "!")
         fieldType = "lava";
 	  else if (ch == "y")
-	    fielfType = "floater";
+	    fieldType = "floater";
 
       // "Push" the fieldType, which is a string, onto the gridLine array (at the end).
       gridLine.push(fieldType);
